f99e13bbe8222e6472ec2384a8ff85e7bcf24715
J. Christian Romero <jcromerohdz@gmail.com>
2019-11-07 11:38:37 -0800

    Adding test models to the project

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@
 
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
   has_many :posts
   has_many :comments
 end
