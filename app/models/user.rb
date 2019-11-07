1a95ba2cf868eadbf96310bcfcd63e99ec867417
J. Christian Romero <jcromerohdz@gmail.com>
2019-11-07 11:41:16 -0800

    Fixing rubocop issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { maximum: 25 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }
   has_many :posts
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,34 +8,34 @@ class UserTest < ActiveSupport::TestCase
   # end
 
   def setup
-    @user = User.new(username: "Example User", email: "user@example.com", password: "mypassword")
+    @user = User.new(username: 'Example User', email: 'user@example.com', password: 'mypassword')
   end
 
-  test "should be valid" do
+  test 'should be valid' do
     assert @user.valid?
   end
 
-  test "name should be present" do
-    @user.username = "     "
+  test 'name should be present' do
+    @user.username = '     '
     assert_not @user.valid?
   end
 
-  test "email should be present" do
-    @user.email = "     "
+  test 'email should be present' do
+    @user.email = '     '
     assert_not @user.valid?
   end
 
-  test "name should not be too long" do
-    @user.username = "a" * 51
+  test 'name should not be too long' do
+    @user.username = 'a' * 51
     assert_not @user.valid?
   end
 
-  test "email should not be too long" do
-    @user.email = "a" * 244 + "@example.com"
+  test 'email should not be too long' do
+    @user.email = 'a' * 244 + '@example.com'
     assert_not @user.valid?
   end
 
-  test "email validation should accept valid addresses" do
+  test 'email validation should accept valid addresses' do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
@@ -44,7 +44,7 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-  test "email validation should reject invalid addresses" do
+  test 'email validation should reject invalid addresses' do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
@@ -52,4 +52,4 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
-end 
+end
